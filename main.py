666606f4b39547fc72287da2c8139d283248a876
pmikola <pio.mikolajczyk@gmail.com>
2024-05-09 15:50:28 +0200

    data preparation ready

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch
 from torch import nn
 
 from model import PINO
-from lerning import teacher
+from teacher import teacher
 
 
 from flameEngine import flame as fl
@@ -17,9 +17,12 @@ torch.manual_seed(2024)
 np.random.seed(2024)
 random.seed(2024)
 
-f1 = fl.flame_sim(no_frames=1500,frame_skip=20)
+
+
 model = PINO()
-t1 = teacher(model)
+t1 = teacher(model,device)
+first_frame,last_frame,frame_skip = 0,200,25
+f1 = fl.flame_sim(no_frames=1000,frame_skip=frame_skip)
 loss = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
 
@@ -28,8 +31,9 @@ for period in range(0,1):
     t1.fsim = f1
     t1.generate_structure()
     t1.fsim.fuel_dens_modifier = 1/t1.fsim.dt
-    t1.fsim.simulate(plot=1)
-    # model = t1(dataloader,loss,optimizer,device)
+    t1.fsim.simulate(simulate=0,save_rgb=1,save_alpha=1,save_fuel=1,delete_data=0)
+    t1.data_preparation(first_frame,last_frame,frame_skip)
+
 
 
 
--- teacher.py
+++ teacher.py
@@ -1,14 +1,18 @@
+import os.path
 import random
 
 import torch
 
 
 class teacher(object):
-    def __init__(self,model):
+    def __init__(self,model,device):
         super(teacher, self).__init__()
         self.model = model
+        self.device = device
         self.fsim = None
-
+        self.data_tensor = None
+        self.meta_tensor = None
+        self.field_names = None
     def generate_structure(self):
         no_structure = random.randint(0, self.fsim.grid_size_y - self.fsim.N_boundary)
         self.fsim.idx = torch.randint(low=self.fsim.N_boundary,high=self.fsim.grid_size_x-self.fsim.N_boundary,size=(no_structure,))
@@ -19,9 +23,33 @@ class teacher(object):
         self.fsim.idy_u = self.fsim.idy
         self.fsim.idx_v = self.fsim.idx
         self.fsim.idy_v = self.fsim.idy
-
     def generate_sim_params(self):
         pass
+
+    def data_preparation(self,first_frame,last_frame,frame_skip):
+        folder_names = ['v','u','velocity_magnitude','fuel_density','oxidizer_density',
+                        'product_density','pressure','temperature','rgb','alpha']
+        data_tensor = []
+        meta_tensor = []
+        field_names = []
+        for name in folder_names:
+            if os.path.exists(name):
+                for i in range(first_frame, last_frame, frame_skip):
+                    if name == 'rgb':
+                        ptfile = torch.load(name+'\\'+'t{}.pt'.format(i))
+                        for j in range(0,3):
+                            data_tensor.append(ptfile['data'][:,:,j])
+                            meta_tensor.append(ptfile['metadata'])
+                            field_names.append(ptfile['name'])
+                    else:
+                        ptfile = torch.load(name+'\\'+'t{}.pt'.format(i))
+                        data_tensor.append(ptfile['data'])
+                        meta_tensor.append(ptfile['metadata'])
+                        field_names.append(ptfile['name'])
+
+        self.data_tensor = torch.stack(data_tensor,dim=0)
+        self.meta_tensor = torch.stack(meta_tensor,dim=0)
+        self.field_names = field_names
 
 def learning_loop(self,dataloader,criterion,optimizer,device,num_epochs=100):
         global loss
